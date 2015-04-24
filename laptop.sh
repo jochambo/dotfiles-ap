f5ec89030de3d88ad1cd077e6727addc8553bdeb
David Laprade <david.laprade@gmail.com>
2015-04-24 19:13:21 -0400

    create setup script
    simplifies the setup process, allowing you to do the job with
    a single command

---
+++ laptop.sh
@@ -0,0 +1,11 @@
+bash brew.sh
+bash boostrap.sh
+bash cask.sh
+
+read -p "WARNING: what follows will install a very opinionated set of OSX configs, do you want to continue? (Y,n)" -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  bash osx.sh
+fi
+
+
