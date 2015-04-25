ce4064f4084bb02ce59d863bd586a3941ef5d963
David Laprade <david.laprade@gmail.com>
2015-04-25 11:36:08 -0400

    cask.sh optional in setup script
    a new developer need not install all of these programs
    and may want to choose among them
    
    also made the response sensitive to case

--- laptop.sh
+++ laptop.sh
@@ -2,11 +2,16 @@
 
 bash brew.sh
 bash boostrap.sh
-bash cask.sh
 
-read -p "WARNING: what follows will install a very opinionated set of OSX configs, do you want to continue? (Y,n)" -n 1
+read -p "WARNING: you are about to install a very opinionated set of OSX applications, do you want to continue? (Y,n)" -n 1
 echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ $REPLY =~ Y ]]; then
+  bash cask.sh
+fi
+
+read -p "WARNING: you are about to install a very opinionated set of OSX configs, do you want to continue? (Y,n)" -n 1
+echo
+if [[ $REPLY =~ Y ]]; then
   bash osx.sh
 fi
 
